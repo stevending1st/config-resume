3b9a21d6cb0e7e37a8b80c4aca81ea4949afb4b5
stevending1st <stevending1st@163.com>
2024-03-05 19:13:10 +0800

    feat(i18n): init i18n

---
+++ packages/i18n/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  format: ['esm'],
+  target: 'node18',
+  splitting: true,
+  dts: true,
+  clean: true,
+  shims: false,
+  external: [/@config-resume/]
+});
