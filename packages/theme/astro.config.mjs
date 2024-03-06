ce0d0e827b3bbf48880bb9a8faff2b9800bca2c3
stevending1st <stevending1st@163.com>
2024-03-06 23:37:48 +0800

    feat(theme): add i18nPlugin, auto change language

--- packages/theme/astro.config.mjs
+++ packages/theme/astro.config.mjs
@@ -1,7 +1,26 @@
 import tailwind from "@astrojs/tailwind";
+import { i18nPlugin } from '@config-resume/cli';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  base: "",
+
+  trailingSlash: 'never',
+
+  integrations: [
+    tailwind(),
+    {
+      name: "config-resume-plugin",
+      hooks: {
+        'astro:config:setup': ({ updateConfig }) => {
+          updateConfig({
+            vite: {
+              plugins: [i18nPlugin()],
+            }
+          })
+        }
+      },
+    },
+  ]
 });
---
+++ packages/theme/src/plugin.d.ts
@@ -0,0 +1,4 @@
+declare module 'virtual:config-resume:i18n' {
+  const I18n: import('@config-resume/i18n').I18nItemExpansionType;
+  export default I18n;
+}
