7c91be601798df4dbc0e4dc5d464bb403356409e
stevending1st <stevending1st@163.com>
2024-03-07 16:00:39 +0800

    fix(cli): fix configResume export

--- packages/cli/src/integrations/configResume.ts
+++ packages/cli/src/integrations/configResume.ts
@@ -3,7 +3,7 @@ import { type AstroIntegration } from 'astro';
 import i18nPlugin from '../plugin/i18n';
 import loadResumePlugin from '../plugin/resume';
 
-export default function configResume(): AstroIntegration {
+export function configResume(): AstroIntegration {
   return {
     name: 'config-resume-plugin',
     hooks: {
@@ -17,3 +17,5 @@ export default function configResume(): AstroIntegration {
     }
   };
 }
+
+export default configResume;
