b9376c3f2cb5469c87c18858a6a4ee3efa8fddf6
stevending1st <stevending1st@163.com>
2024-03-11 12:35:04 +0800

    fix(theme): repair the directory

--- packages/theme/tailwind.config.mjs
+++ packages/theme/tailwind.config.mjs
@@ -1,6 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		path.join(__dirname,'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'),
+	],
 	theme: {
 		extend: {},
 	},
