f2fb74cdde785911c4fc5ffdfbcfdb5ecf54b583
Steven Ding <stevending1st@163.com>
2024-03-28 15:05:01 +0800

    feat: add script for update package version (#5)

---
+++ script/update-versions.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env zx
+
+import { join } from 'node:path';
+
+import { fs } from 'zx';
+
+const templates = [
+  'packages/cli/template',
+]
+
+const { version } = await fs.readJSON('package.json')
+
+for (const template of templates) {
+  const path = join(template, 'package.json');
+
+  if(!(await fs.exists(path))) continue;
+
+  const pkg = await fs.readJSON(path)
+  const deps = ['dependencies', 'devDependencies']
+
+  for (const name of deps) {
+    if (!pkg[name]) continue;
+
+    const pkgMap = new Map(pkg[name]);
+    for(const [key] of pkgMap) {
+      if(key.startsWith('@config-resume')) pkgMap.set(key, version);
+    }
+    pkg[name] = Object.fromEntries(pkgMap.entries());
+  }
+
+  await fs.writeJSON(path, pkg, { spaces: 2 });
+}
